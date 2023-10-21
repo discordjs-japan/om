3cfd4ca1ac478a29fa6d0faa3d8c1088951c5807
femshima <49227365+femshima@users.noreply.github.com>
2023-10-21 13:05:42 +0900

    common build options

--- build.js
+++ build.js
@@ -1,22 +1,22 @@
 import { build } from "esbuild";
 import packageJson from "./package.json" assert { type: "json" };
 
-await build({
-  entryPoints: ["src/main.ts"],
+const buildOptions = {
   bundle: true,
-  outfile: "dist/main.js",
   platform: "node",
   format: "esm",
   external: Object.keys(packageJson.dependencies),
   minify: true,
+}
+
+await build({
+  ...buildOptions,
+  entryPoints: ["src/main.ts"],
+  outfile: "dist/main.js",
 });
 
 await build({
+  ...buildOptions,
   entryPoints: ["src/synthesis/task.ts"],
-  bundle: true,
   outfile: "dist/task.js",
-  platform: "node",
-  format: "esm",
-  external: Object.keys(packageJson.dependencies),
-  minify: true,
 });
