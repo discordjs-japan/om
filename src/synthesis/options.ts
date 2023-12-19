386aff81fa35434c3bf27858fc1cb89c3a9e22b7
cm-ayf <cm.ayf2734@gmail.com>
2023-12-19 21:45:39 +0900

    feat: return varied synthesis options by primitive rng (#76)
    createSynthesisOptionにて，`message.author.id`を用いて声がある程度ばらけるように実装しました．同じ`message.author.id`に対しては同じ`SynthesisOption`を返します．
    
    ---------
    
    Co-authored-by: phenylshima <49227365+femshima@users.noreply.github.com>

--- src/synthesis/options.ts
+++ src/synthesis/options.ts
@@ -1,9 +1,40 @@
 import { Message } from "discord.js";
 import { SynthesisOption } from "node-altjtalk-binding";
 
-export function createSynthesisOption(_: Message): SynthesisOption {
+class SeededRng {
+  private static readonly A = 48271n;
+  private static readonly M = 2147483647n;
+
+  private x: bigint;
+
+  constructor(seed: string) {
+    try {
+      this.x = BigInt(seed);
+    } catch {
+      this.x = 0n;
+    }
+  }
+
+  next() {
+    this.x = (SeededRng.A * this.x) % SeededRng.M;
+  }
+
+  /**
+   * @param exponent must be positive odd integer. the bigger, the more likely to be near mean.
+   */
+  range(mean: number, radius: number, exponent = 1): number {
+    this.next();
+    const disk = (2 * Number(this.x)) / Number(SeededRng.M) - 1;
+    return mean + disk ** exponent * radius;
+  }
+}
+
+export function createSynthesisOption(message: Message): SynthesisOption {
+  const rng = new SeededRng(message.author.id);
+
   return {
-    additionalHalfTone: -5,
-    speechSpeedRate: 1.2,
+    additionalHalfTone: rng.range(-4, 3),
+    speechSpeedRate: rng.range(1.2, 0.3, 3),
+    weightOfGvForLogF0: Math.exp(rng.range(0, 1, 3)),
   };
 }
