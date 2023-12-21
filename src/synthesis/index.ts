c9975266824652cf2a222945a4255fc9147a8c6f
cm-ayf <cm.ayf2734@gmail.com>
2023-12-21 22:31:12 +0900

    fix: change default dictionary/model path to relative (#83)

--- src/synthesis/index.ts
+++ src/synthesis/index.ts
@@ -2,7 +2,6 @@ import { Synthesizer } from "./synthesizer";
 import WorkerSynthesizer from "./worker-synthesizer";
 
 export const synthesizer: Synthesizer = new WorkerSynthesizer(
-  process.env.DICTIONARY ?? "/app/model/naist-jdic",
-  process.env.MODEL ??
-    "/app/model/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice",
+  process.env.DICTIONARY ?? "model/naist-jdic",
+  process.env.MODEL ?? "model/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice",
 );
