524b4f8e480d1c163a159ec7b0d04df448265b91
cm-ayf <cm.ayf2734@gmail.com>
2023-12-23 10:39:27 +0900

    add synthesis/options/seeded-rng.test.ts

--- src/synthesis/options/seeded-rng.ts
+++ src/synthesis/options/seeded-rng.ts
@@ -1,7 +1,4 @@
-import { Message } from "discord.js";
-import { SynthesisOption } from "node-altjtalk-binding";
-
-class SeededRng {
+export default class SeededRng {
   private static readonly A = 48271n;
   private static readonly M = 2147483647n;
 
@@ -27,14 +24,4 @@ class SeededRng {
     const disk = (2 * Number(this.x)) / Number(SeededRng.M) - 1;
     return mean + disk ** exponent * radius;
   }
-}
-
-export function createSynthesisOption(message: Message): SynthesisOption {
-  const rng = new SeededRng(message.author.id);
-
-  return {
-    additionalHalfTone: rng.range(-4, 3),
-    speechSpeedRate: rng.range(1.2, 0.3, 3),
-    weightOfGvForLogF0: Math.exp(rng.range(0, 1, 3)),
-  };
 }
