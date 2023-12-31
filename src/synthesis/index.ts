cd98e021866619944976c5e484862e8666618acc
femshima <49227365+femshima@users.noreply.github.com>
2024-01-01 07:58:54 +0900

    fix docker build

--- src/synthesis/index.ts
+++ src/synthesis/index.ts
@@ -15,9 +15,7 @@ function parseStringArray(arg: string) {
 
 export const synthesizer: Synthesizer = new WorkerSynthesizer(
   process.env.DICTIONARY ?? "model/naist-jdic",
-  process.env.DICTIONARY
-    ? process.env.USER_DICTIONARY
-    : "model/user-dictionary.bin",
+  process.env.USER_DICTIONARY,
   process.env.MODEL
     ? parseStringArray(process.env.MODEL)
     : ["model/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice"],
