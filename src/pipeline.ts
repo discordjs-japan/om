cd96f3190bd6fcc14bffbcbbf9e3cca50bfd7b87
NishiOwO <89888985+NishiOwO@users.noreply.github.com>
2024-06-02 23:41:45 +0900

    feat: Don't start speaking when humans are speaking (#412)

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -70,6 +70,10 @@ export default class Pipeline extends EventEmitter {
           return this.emit("destroy");
       }
     });
+    this.connection.receiver.speaking.on("end", () => {
+      // wait for ...speaking.users to be updated before this.play()
+      setImmediate(() => this.play());
+    });
     this.player.on("stateChange", (_, newState) => {
       switch (newState.status) {
         case AudioPlayerStatus.Idle:
@@ -114,6 +118,7 @@ export default class Pipeline extends EventEmitter {
   play() {
     if (this.connection?.state.status !== VoiceConnectionStatus.Ready) return;
     if (this.player?.state.status !== AudioPlayerStatus.Idle) return;
+    if (this.isHumanSpeaking()) return;
     const audio = this.audioQueue.shift();
     if (!audio) return;
     this.playing = audio;
@@ -131,6 +136,12 @@ export default class Pipeline extends EventEmitter {
       !this.connection ||
       this.connection.state.status === VoiceConnectionStatus.Disconnected ||
       this.connection.state.status === VoiceConnectionStatus.Destroyed
+    );
+  }
+
+  isHumanSpeaking() {
+    return new Collection(this.connection?.receiver.speaking.users).some(
+      (epoch, id) => this.channel.client.users.cache.get(id)?.bot === false,
     );
   }
 
